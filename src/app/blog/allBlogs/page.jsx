9e2955219d1f86f60a51eccf04f97f2e73aef933
mohamed-magdy-1 <King44123no@gamil.com>
2025-02-12 22:13:11 +0200

    New Updated

--- src/app/blog/allBlogs/page.jsx
+++ src/app/blog/allBlogs/page.jsx
@@ -9,6 +9,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import DOMPurify from 'dompurify';
 import dynamic from 'next/dynamic';
+import Loading2 from '@/app/components/loading-2/loading-2';
 
 const LordIconDocument = dynamic(() => import('../../components/LordIcon/LordIcon').then((mod) => mod.LordIconDocument), { ssr: false });
 const LordIconGlobe = dynamic(() => import('../../components/LordIcon/LordIcon').then((mod) => mod.LordIconGlobe), { ssr: false });
@@ -17,18 +18,22 @@ export default function AllBlogs() {
   const [data, setData] = useState(null);
   const [pageCount, setPageCount] = useState();
   const [pageIndex, setPageIndex] = useState(1);
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function AllBlogsBlogs() {
       try {
+        setLoading(true);
         const res = await GlobalApi.AllBlogsApi(pageIndex);
         if (res?.data) {
           setData(res.data);
           setPageCount(res?.meta?.pagination?.pageCount);
+          setLoading(false);
         }
       } catch (err) {
         console.log(err);
+      } finally {
+        setLoading(false);
       }
     }
     AllBlogsBlogs();
@@ -42,7 +47,10 @@ export default function AllBlogs() {
     <>
       <Header title={"AllBlogs"} />
       <div className="allBlogs">
-        {data?.map((item) => (
+                            {loading ? (<Loading2/>) 
+                            : (
+                              <>
+                                      {data?.map((item) => (
           <Link key={item.id} className="allBlogs-card" href={`/blog/${item?.slug}`}>
               <div className="title">
                 <h2>{item.title}</h2>
@@ -51,10 +59,10 @@ export default function AllBlogs() {
                 </h4>
               </div>
             <div className="allBlogs-img">
-              {item?.cover[0]?.url && (
+              {item?.cover?.url && (
                 <Image
                   className="img-1"
-                  src={item?.cover[0]?.url}
+                  src={item?.cover?.url}
                   alt="Background Image"
                   width={400}
                   height={300}
@@ -80,6 +88,9 @@ export default function AllBlogs() {
             </div>
           </Link>
         ))}
+                              </>
+                            )}
+
       </div>
       <DynamicPagination pageCount={pageCount} pageIndex={pageIndex} setPageIndex={setPageIndex} />
     </>
